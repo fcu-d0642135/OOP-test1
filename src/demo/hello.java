0af45ce8c12ebb7cabd35e9078433fbca9be1b1d
shan <sh43204327@gmail.com>
2019-02-21 14:49:48 +0800

    把 world 改成 FCU

--- src/demo/hello.java
+++ src/demo/hello.java
@@ -1,10 +1,9 @@
 package demo;
 
 public class hello {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		System.out.println("Hello FCU!");
 	}
 
 }
